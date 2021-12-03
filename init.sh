94a893cec559c2f365bacb5f0330523b6e9c2e8f
Daniel Koch <dkoch84@gmail.com>
2021-12-03 17:37:31 -0600

    alacritty added

--- init.sh
+++ init.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-cp autostart ~/.config/herbstluftwm/
-cp .bashrc ~/
-cp .vimrc ~/
-cp config.py ~/.config/qutebrowser/
-cp redshift.conf ~/.config/redshift
-cp .Xresources ~/
-cp config ~/.ncmpcpp/
-cp mpd.conf ~/.config/mpd/
+cp herbstluftwm/autostart ~/.config/herbstluftwm/
+cp bash/.bashrc ~/
+cp vim/.vimrc ~/
+cp qutebrowser/config.py ~/.config/qutebrowser/
+cp redshift/redshift.conf ~/.config/redshift
+cp xorg/.Xresources ~/
+cp ncmpcpp/config ~/.ncmpcpp/
+cp mpd/mpd.conf ~/.config/mpd/
+cp tint2/tint2rc ~/.config/tint2/
+cp alacritty/alacritty.yml ~/.config/alacritty/
 
 printf "dotfiles applied\n"
 exit 0
